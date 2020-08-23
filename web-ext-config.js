7bfe45236aca038c00151a215c75d95bf0d69c51
Eric King <eric.king@asurion.com>
2020-08-23 17:09:48 -0500

    Updated start url

--- web-ext-config.js
+++ web-ext-config.js
@@ -8,7 +8,10 @@ module.exports = {
     overwriteDest: true,
   },
   run: {
-    startUrl: [ 'about:debugging' ],
+    startUrl: [
+      'https://sonash-kibana.dev.soluto.npr.aws.asurion.net/_plugin/kibana/app/kibana#/discover?_g=(refreshInterval:(display:Off,pause:!f,value:0),time:(from:now-4h,mode:quick,to:now))&_a=(columns:!(_source),index:AWfq8r7CEXT7aK6X1o5n,interval:auto,query:(match_all:()),sort:!(\'@timestamp\',desc))',
+      'about:debugging',
+    ],
   },
   ignoreFiles: [ 'package-lock.json' ],
 };
