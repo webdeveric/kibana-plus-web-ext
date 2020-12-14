caf0a1c2c803803c3e25c24a573a86eee7187968
Eric King <eric.king@asurion.com>
2020-12-14 16:18:37 -0600

    Added createLambdaAnchor

--- src/contentScript/replacers/createLambdaArnAnchor.ts
+++ src/contentScript/replacers/createLambdaArnAnchor.ts
@@ -3,17 +3,47 @@ import { makeTextReplacer } from '../replacer';
 
 import styles from '../main.css';
 
+function getAwsLambdaUrl({
+  functionName,
+  region,
+}: {
+  functionName: string;
+  region?: string;
+}): URL {
+  const url = new URL('https://console.aws.amazon.com/lambda/home');
+
+  url.hash = `/functions/${functionName}`;
+
+  if ( region ) {
+    url.searchParams.append('region', region);
+  }
+
+  return url;
+}
+
 export const createLambdaArnAnchor = makeTextReplacer(
-  /\barn:aws:lambda:(?<region>[^:]+):\d+:function:(?<name>[a-z0-9-:]+)\b/i,
+  /\barn:aws:lambda:(?<region>[^:]+):\d+:function:(?<functionName>[a-z0-9-:]+)\b/i,
   (fragment: DocumentFragment, match?: RegExpMatchArray) : Element | undefined => {
     if ( match ) {
-      const { name, region } = match.groups ?? {};
+      const { functionName, region } = match.groups ?? {};
 
-      if ( name && region ) {
-        const url = new URL('https://console.aws.amazon.com/lambda/home');
+      if ( functionName && region ) {
+        const url = getAwsLambdaUrl({ functionName, region });
 
-        url.searchParams.append('region', region);
-        url.hash = `/functions/${name}`;
+        return createAnchor( url.toString(), fragment, styles.anchor, { title: 'View on AWS' } );
+      }
+    }
+  }
+);
+
+export const createLambdaAnchor = makeTextReplacer(
+  /(?<="functionName":\s")(?<functionName>.+)(?=")/,
+  (fragment: DocumentFragment, match?: RegExpMatchArray) : Element | undefined => {
+    if ( match ) {
+      const { functionName } = match.groups ?? {};
+
+      if ( functionName ) {
+        const url = getAwsLambdaUrl({ functionName });
 
         return createAnchor( url.toString(), fragment, styles.anchor, { title: 'View on AWS' } );
       }
